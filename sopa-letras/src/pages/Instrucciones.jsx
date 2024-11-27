89e8db414bb7ad2a5fbe27389517b8b3ddc9aa1c
Juan Zambrano <jdzambranom@libertadores.edu.co>
2024-11-27 23:36:54 +0000

    Cambios en pages Instrucciones

--- sopa-letras/src/pages/Instrucciones.jsx
+++ sopa-letras/src/pages/Instrucciones.jsx
@@ -8,10 +8,20 @@ const Instrucciones = () => {
   return (
     <Portada>
       <Texto content="¿Cómo se juega?" type="h1" />
-      <Texto content="Busca las dos palabras en la sopa de letras ¡Es muy sencillo!" type="p" />
-      <Texto content="Lee la historia y clikea en la  flecha para continuar con la historia" type="p" />
-      <Boton text="¡A leer!" color="green" route="/fabula" />
-      
+      <Texto 
+        content="Busca las dos palabras en la sopa de letras ¡Es muy sencillo!" 
+        type="p" 
+      />
+      <Texto 
+        content="Lee la historia y haz clic en la flecha para continuar con la historia" 
+        type="p" 
+      />
+      <Boton 
+        text="¡A leer!" 
+        color="#7D50A6"
+        route="/fabula" 
+        aria-label="Ir a la fábula" 
+      />
     </Portada>
   );
 };
